e31b0181fdb27d214475ab5dfbe516f6dae789e2
GProject-dev <dkstmdwns67@gmail.com>
2019-12-03 22:40:32 +0900

    code

---
+++ code/code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void move(int *xpos, int *ypos);
+int main(void)
+{
+	int x = 10;
+	int y = 20;
+	printf("%d %d\n", x, y);
+	move(&x, &y);
+	printf("%d %d\n", x, y);
+
+	return 0;
+}
+
+void move(int *xpos, int *ypos)
+{
+	*xpos = *xpos + 1;
+	*ypos = *ypos + 1;
+
+
+}
